d84db6af751e6b5f0e1bed520f825af85a1dd41d
SierraAntonio <122294770+SierraAntonio@users.noreply.github.com>
2023-01-24 20:19:35 -0600

    Add files via upload
    Aqui se adjuntan los datos que fueron incluidos en el siwtch

---
+++ datosdelswitch.java
@@ -0,0 +1,40 @@
+package area;
+import java.util. Scanner;
+
+public class rectangulo1 {
+Scanner in = new Scanner(System.in);
+	int a,b,r,p,ra, cir,ar,diago;
+	double diag = Math.sqrt(diago);
+	public void perimetrodelrectangulo() {
+		p=(2*a)+(2*b);
+		System.out.println("El perimetro del rectangulo es: " + p);
+	}
+		
+public void areadelrectangulo() {
+	r=a*b;
+	System.out.println("El area del rectangulo es: " + r);
+	}
+public void circunferenciadelcirculo() {
+	cir=(2*ra)*3;
+	System.out.println("La circunferencia del circulo es: " + cir);
+	}
+public void areaciadelcirculo() {
+	ar=3*(ra*ra);
+	System.out.println("El area del circulo es: " + ar);
+}
+public void diagonaldelrectangulo() {
+
+	System.out.println("La diagonal del circulo " + diag);
+}
+public void datos() {
+	System.out.println("Ingresa Altura");
+	a=in.nextInt();
+	System.out.println("Ingresa Base");
+	b=in.nextInt();
+}
+public void datos2() {
+	System.out.println("Ingresa el radio del circulo");
+	ra=in.nextInt();
+	
+}
+}
